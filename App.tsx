6f81d0bb79d6eca96f9e778fcac9a9421ab2ba99
Gdempc <grzegorz.dempc@gmail.com>
2023-05-15 11:38:36 +0200

    sccommit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <View style={styles.top}>
+        <TouchableOpacity style={styles.new}>
+          <Text style={styles.size}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.note}>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.note}>
+        
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.note}>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.note}>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.note}>
+
+      </TouchableOpacity>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  top: {
+    width: "100%",
+    height: "10%",
+    backgroundColor: '#29a0cf',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  new:{
+    margin: 0,
+    position: 'absolute',
+    top: '15%',
+    right: '2%',
+    width:50,
+    height: 50,
+    borderRadius:50,
+    backgroundColor: "#ffff00",
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  note:{
+    width:"95%",
+    height: "15%",
+    backgroundColor: "#aaaaaa",
+    margin: 10,
+    borderRadius: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  size:{
+    fontSize: 50,
+    marginLeft : 'auto',
+    marginRight:'auto',
+    top:-10
   },
 });
 
